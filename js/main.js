17568dadb7c3257812d930a92f5570fdf2aa8965
Vasyl <vasulkydruch@gmail.com>
2022-06-05 14:53:20 +0300

    another commit

--- js/main.js
+++ js/main.js
@@ -18,3 +18,54 @@ $(document).ready(function () {
         $("body").toggleClass("lock");
     });
 });
+
+$(document).ready(function(){
+
+    let $containerWidth = $(window).width();
+
+    if ($containerWidth <= 767)  {
+
+        // $(".section-counties-item__title--active").click(function(){
+        //     $(".sub-menu").slideToggle();
+        // });
+        //
+        // $(".section-counties-item__title").hover(
+        //     function() {
+        //         $(this).toggleClass('section-counties-item__title--active')
+        //     }
+        // );
+
+        $(".section-counties-item__title--US").click(function(){
+            $(".sub-menu--US").slideToggle();
+        });
+
+        $(".section-counties-item__title--AF").click(function(){
+            $(".sub-menu--AF").slideToggle();
+        });
+
+        $(".section-counties-item__title--SA").click(function(){
+            $(".sub-menu--SA").slideToggle();
+        });
+
+        $(".section-counties-item__title--EU").click(function(){
+            $(".sub-menu--EU").slideToggle();
+        });
+
+        $(".section-counties-item__title--CA").click(function(){
+            $(".sub-menu--CA").slideToggle();
+        });
+
+        $(".section-counties-item__title--SB").click(function(){
+            $(".sub-menu--SB").slideToggle();
+        });
+
+        $(".section-counties-item__title--OU").click(function(){
+            $(".sub-menu--OU").slideToggle();
+        });
+
+        $(".section-counties-item__title--A").click(function(){
+            $(".sub-menu--A").slideToggle();
+        });
+
+    } else return 0;
+});
